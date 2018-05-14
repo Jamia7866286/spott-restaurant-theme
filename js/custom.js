16418d502bc04e46ae040f8321fe566b6234e8d9
Ammmmmmmir <naseemahmadjmi96>
2018-05-14 17:01:00 +0530

    spot/restaurant theme

---
+++ js/custom.js
@@ -0,0 +1,62 @@
+jQuery(document).ready(function(){
+    // This button will increment the value
+    $('.qtyplus').click(function(e){
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        fieldName = $(this).attr('field');
+        // Get its current value
+        var currentVal = parseInt($('input[name='+fieldName+']').val());
+        // If is not undefined
+        if (!isNaN(currentVal)) {
+            // Increment
+            $('input[name='+fieldName+']').val(currentVal + 1);
+        } else {
+            // Otherwise put a 0 there
+            $('input[name='+fieldName+']').val(0);
+        }
+    });
+    // This button will decrement the value till 0
+    $(".qtyminus").click(function(e) {
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        fieldName = $(this).attr('field');
+        // Get its current value
+        var currentVal = parseInt($('input[name='+fieldName+']').val());
+        // If it isn't undefined or its greater than 0
+        if (!isNaN(currentVal) && currentVal > 0) {
+            // Decrement one
+            $('input[name='+fieldName+']').val(currentVal - 1);
+        } else {
+            // Otherwise put a 0 there
+            $('input[name='+fieldName+']').val(0);
+        }
+    });
+	
+	var accordion = document.getElementsByClassName('custom-accordion');
+    for (var i = 0; i < accordion.length; i++) {
+        accordion[i].onclick = function() {
+            this.classList.toggle('active');
+            var change = this.nextElementSibling;
+            if (change.style.maxHeight) {
+                change.style.maxHeight = null;
+            } else {
+                change.style.maxHeight = change.scrollHeight + 'px';
+            }
+        }
+    }
+	
+	var sh = document.getElementsByClassName('accordion');
+    for (var i = 0; i < sh.length; i++) {
+        sh[i].onclick = function() {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        }
+    }
+});
